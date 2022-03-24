8c49027fa5787c0de216e092eec54988e93b5aa7
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-03-24 13:17:40 +0700

    challenge

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ const fs = require("fs");
 
 // CORS = Cross Origin Resource Sharing
 
+// Challenge
+// 1. Buat CRUD API untuk sebuah todo list
+// 2. Terapkan logging menggunakan middleware dengan format sbg berikut:
+//    "<HTTP_METHOD> <PATH> <hh:mm DD/MM/YYYY>"
+//    "GET /todos 13:01 24/03/2022"
+// 3. Logs harus muncul di console/terminal setiap request
+// 4. Logs harus disimpan ke dalam file bernama ".log"
+// 5. Tampilkan todo list di front end (next atau create-react-app)
+// 6. Connect todo list frontend ke back end
+
+// Isi todo list
+// id: generate secara random
+// status: "done" || "not done"
+// action: string
+
 dotenv.config();
 
 app.use(cors())
